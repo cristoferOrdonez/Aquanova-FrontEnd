b296d2d40f93f87b38098de56b71aa5f2ec741c2
faridardila <deardilah@unal.edu.co>
2026-01-11 23:59:52 -0500

    implementación página de lista de formularios

---
+++ Aquanova/src/components/FormList.jsx
@@ -0,0 +1,31 @@
+import FormCard from "./FormCard"
+import { PlusIcon } from "@heroicons/react/24/outline"
+import SearchBar from "./SearchBar"
+
+function FormList(){
+  return(
+    <div className="p-4">
+      <div className="flex flex-row flex-1">
+        <div className="w-full">
+          <SearchBar/>
+        </div>
+
+        <button className="flex flex-row gap-3 whitespace-nowrap bg-[var(--blue-buttons)] rounded-4xl font-work text-white justify-center items-center p-3">
+          <PlusIcon className="h-10 w-10"/>
+          Nueva campaña
+        </button>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-10 mt-5">
+        <FormCard/>
+        <FormCard/>
+        <FormCard/>
+        <FormCard/>
+        <FormCard/>
+        <FormCard/>
+      </div>
+    </div>
+    
+  )
+}
+
+export default FormList
